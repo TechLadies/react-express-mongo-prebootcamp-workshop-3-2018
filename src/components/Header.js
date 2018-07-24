cff77bd156612677f39b84e7f1021ae5d66448fd
tjjjjwxzq <aquietimmanence@gmail.com>
2018-07-24 18:29:15 +0800

    Add links to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export class Header extends React.Component {
-    render() {
-        return (
-            <header>
-                <h1>My Amazing Todo App</h1>
-            </header>
-        )
-    }
+  render() {
+    return (
+      <header>
+        <h1>My Amazing Todo App</h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/todos">Todos</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    )
+  }
 }
