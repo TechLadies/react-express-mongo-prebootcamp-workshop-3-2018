820967c26ef9f32c108015dbb9bb779ad6bc50bb
tjjjjwxzq <aquietimmanence@gmail.com>
2018-07-28 15:33:10 +0800

    Add functionality to add new todos

--- src/components/App.js
+++ src/components/App.js
@@ -12,8 +12,16 @@ class App extends React.Component {
         description: "buy oranges",
         isDone: true
       }
-    ]
+    ],
+    newTodoDescription: "",
   };
+
+  handleOnChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value,
+    });
+  }
 
   handleTodoClick = currentTodo => {
     if (currentTodo.isDone) {
@@ -29,22 +37,52 @@ class App extends React.Component {
     this.setState(updatedState);
   };
 
+
+  handleAddTodo = () => {
+    // Step 1: get new todo description
+    const newTodoDescription = this.state.newTodoDescription
+    // Step 2: create new todo object from the description
+    const newTodo = {
+      description: newTodoDescription,
+      isDone: false,
+    };
+    // Step 3: update react component state
+    const newTodos = [
+      ...this.state.todos,
+      newTodo,
+    ];
+
+    this.setState({
+      todos: newTodos,
+    });
+  }
+
   render() {
     return (
       <div>
         <h1>My awesome todo list</h1>
+        <label htmlFor="newTodoDescription">Add Todo</label>
+        <input
+          type="text"
+          value={this.state.newTodoDescription}
+          name="newTodoDescription"
+          id="newTodoDescription"
+          onChange={this.handleOnChange}
+        />
+        <button onClick={this.handleAddTodo}>+</button>
+
         <ul>
-          {this.state.todos.map(a => {
+          {this.state.todos.map(todo => {
             let completeClass = "";
-            if (a.isDone) {
+            if (todo.isDone) {
               completeClass = "complete";
             }
             return (
               <li
                 className={completeClass}
-                onClick={() => this.handleTodoClick(a)}
+                onClick={() => this.handleTodoClick(todo)}
               >
-                {a.description}
+                {todo.description}
               </li>
             );
           })}
